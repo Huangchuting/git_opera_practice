a0872b08059a1ee6f12e3a164d9ae26d66e8fe65
Huangchuting <759040635@qq.com>
2020-02-20 11:15:45 +0800

    fix: css demo 伪类

--- src/main/cssDemo/views/demo.vue
+++ src/main/cssDemo/views/demo.vue
@@ -26,14 +26,23 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="demo">
-            <h5>3.随机粒子动画</h5>
+        <div class="demo">
+            <h5>3.伪类</h5>
             <div class="demo-3">
-                <div class="snow-wrap">
-                    <span v-for="(item, index) in  200"  :key="index" class="snow"></span>
-                </div>
+                <button
+                    data-text="Start"
+                    class="btn btn-primary btn-ghost btn-border-stroke  btn-text-float-up"
+                    >
+                    <div class="btn-borders">
+                        <div class="border-top"></div>
+                        <div class="border-right"></div>
+                        <div class="border-bottom"></div>
+                        <div class="border-left"></div>
+                    </div>
+                    <span class="btn-text">Start</span>
+                </button>
             </div>
-        </div> -->
+        </div>
     </div>
 </template>
 <script>
@@ -202,6 +211,186 @@ h5{color: #fff;font-size: 20px;margin: 30px 0 10px;}
             right: -6%;
             opacity: 1;
             transform: scale(0.9);
+        }
+    }
+}
+.demo-3{
+    .btn {
+        $hue: 190;
+        $ease-in-duration: 0.25s;
+        $ease-in-exponential: cubic-bezier(0.95, 0.05, 0.795, 0.035);
+        $ease-out-duration: 0.65s;
+        $ease-out-delay: $ease-in-duration;
+        $ease-out-exponential: cubic-bezier(0.19, 1, 0.22, 1);
+        position: relative;
+        padding: 1rem 3rem;
+        font-size: 1rem;
+        line-height: 1.5;
+        color: white;
+        text-decoration: none;
+        background-color: hsl($hue, 100%, 41%);
+        border: 1px solid hsl($hue, 100%, 41%);
+        outline: transparent;
+        overflow: hidden;
+        cursor: pointer;
+        user-select: none;
+        white-space: nowrap;
+        transition: 0.25s;
+
+        &:hover {
+            background: hsl($hue, 100%, 31%);
+        }
+
+        &-primary {
+            $hue: 171;
+        }
+
+        &-ghost {
+            color: hsl($hue, 100%, 41%);
+            background-color: transparent;
+            border-color: hsl($hue, 100%, 41%);
+
+            &:hover {
+                color: white;
+            }
+        }
+
+        &-border-stroke {
+            border-color: hsla($hue, 100%, 41%, 0.35);
+
+            .btn-borders {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+
+                .border-top {
+                    position: absolute;
+                    top: 0;
+                    width: 100%;
+                    height: 1px;
+                    background: hsl($hue, 100%, 41%);
+                    transform: scaleX(0);
+                    transform-origin: left;
+                }
+
+                .border-right {
+                    position: absolute;
+                    right: 0;
+                    width: 1px;
+                    height: 100%;
+                    background: hsl($hue, 100%, 41%);
+                    transform: scaleY(0);
+                    transform-origin: bottom;
+                }
+
+                .border-bottom {
+                    position: absolute;
+                    bottom: 0;
+                    width: 100%;
+                    height: 1px;
+                    background: hsl($hue, 100%, 41%);
+                    transform: scaleX(0);
+                    transform-origin: left;
+                }
+
+                .border-left {
+                    position: absolute;
+                    left: 0;
+                    width: 1px;
+                    height: 100%;
+                    background: hsl($hue, 100%, 41%);
+                    transform: scaleY(0);
+                    transform-origin: bottom;
+                }
+
+                // when unhover, ease-out left, bottom; ease-in right, top
+
+                .border-left {
+                    transition: $ease-out-duration $ease-out-delay $ease-out-exponential;
+                }
+                .border-bottom {
+                    transition: $ease-out-duration $ease-out-delay $ease-out-exponential;
+                }
+
+                .border-right {
+                    transition:  $ease-in-exponential;
+                }
+                .border-top {
+                    transition:  $ease-in-exponential;
+                }
+            }
+
+            &:hover {
+                color: hsl($hue, 100%, 41%);
+                background: transparent;
+
+                .border-top,
+                .border-bottom {
+                    transform: scaleX(1);
+                }
+                .border-left,
+                .border-right {
+                    transform: scaleY(1);
+                }
+
+                // when hover, ease-in left, bottom; ease-out right, top
+
+                .border-left {
+                    transition:  $ease-in-exponential;
+                }
+                .border-bottom {
+                    transition:  $ease-in-exponential;
+                }
+
+                .border-right {
+                    transition: $ease-out-duration $ease-out-delay $ease-out-exponential;
+                }
+                .border-top {
+                    transition: $ease-out-duration $ease-out-delay $ease-out-exponential;
+                }
+            }
+        }
+
+        &-text-float-up {
+            &::after {
+                position: absolute;
+                content: attr(data-text);
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                opacity: 0;
+                transform: translateY(35%);
+                transition: 0.25s ease-out;
+            }
+
+            // when hover, ease-in top-text; ease-out bottom-text
+
+            .btn-text {
+                display: block;
+                transition: 0.75s 0.1s $ease-out-exponential;
+            }
+
+            &:hover {
+                // when hover, ease-in bottom-text; ease-out top-text
+
+                .btn-text {
+                    opacity: 0;
+                    transform: translateY(-25%);
+                    transition: 0.25s ease-out;
+                }
+
+                &::after {
+                    opacity: 1;
+                    transform: translateY(0);
+                    transition: 0.75s 0.1s $ease-out-exponential;
+                }
+            }
         }
     }
 }
